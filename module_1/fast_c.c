6228a10a6962a5e69e2a3ccdc241aa836965ef67
Abtahi <163572318+abtahi2024@users.noreply.github.com>
2025-03-23 21:46:07 +0600

    Add files via upload

---
+++ module_1/fast_c.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    long int a,b,c;scanf("%ld %ld %ld",&a,&b,&c);
+    int sum=a+b+c;
+    printf("%ld\n",sum);
+    printf("hallo phitron!\n");
+    return 0;
+}
